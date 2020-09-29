bef4682ea5df5c85d971f0174cfb85595ba8482e
whoisnegrello <lucionegrello@yahoo.com.ar>
2020-09-29 16:23:26 -0300

    Agregué router de login

--- src/network/routes.js
+++ src/network/routes.js
@@ -1,7 +1,9 @@
+const routerAuth = require('../components/auth/network');
 const routerPosts = require('../components/posts/network');
 const routerUsers = require('../components/users/network');
 
 const routes = function (server) {
+    server.use('/login', routerAuth);
     server.use('/posts', routerPosts);
     server.use('/users', routerUsers);
 }
